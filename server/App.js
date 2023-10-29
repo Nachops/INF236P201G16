81f7f58a4d49bd016dddeac55d2c610715ce17e0
TheSonGori <javiera.gutierreza@usm.cl>
2023-10-29 20:40:00 -0300

    H4

---
+++ server/App.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const db = require("./Database");
+
+const app = express();
+
+app.set("port", process.env.PORT || 5000);
+
+//Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+db();
+
+app.listen(app.get("port", () =>{
+    console.log('Servidor esta corriendo en el puerto: ${app.get("port")}');
+}));
+
+module.exports = app;
