898d5cfd8ac0c3137028abb10d5795e78b05cf0d
Martín Pino Cornejo <martin.pino@sansano.usm.cl>
2023-11-25 23:41:50 -0300

    prueba user

--- mem-stack/server/App.js
+++ mem-stack/server/App.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-
-const db = require('./Database');
-
+const db = require("./Database") 
 const app = express();
+const userRoutes = require("./Routes/pacientes")
+const horarioERoutes = require("./Routes/horario_examenes")
+app.set ("port", process.env.PORT || 5000);
 
-app.set("port", process.env.PORT || 5000);
+//Middlewares
 
-// Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
-
 db();
 
-app.listen(app.get("port"), () => {
-    console.log(`Servidor esta corriendo en el puerto: ${app.get("port")}`);
+app.use('/api', userRoutes);
+app.use('/api', horarioERoutes);
+
+app.get("/", (req,res)=>{
+    res.send("Welcome to my API")
 });
 
-module.exports = app;
+
+app.listen(app.get("port"), () =>{
+    console.log(`Servidor esta corriendo en el puerto: ${app.get("port")}`);
+});
+                         
+module.exports=app;
